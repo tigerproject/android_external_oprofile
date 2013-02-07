3c56fd3fb85695640331d820057688be973602b3
Kejar31 <Kejar31@gmail.com>
2013-02-07 13:17:58 -0600

    fixed typo

--- common.mk
+++ common.mk
@@ -36,7 +36,7 @@ HAVE_LIBBFD := false
 
 # When building with a non-prebuilt toolchain, oprofile complains about
 # missing includes for libbfd
-ifneq ($(findstring linaro,$(TARGET_TOOLS_PREFIX)),linaro)
+ifeq ($(findstring linaro,$(TARGET_TOOLS_PREFIX)),linaro)
 ifeq ($(TARGET_ARCH),arm)
 toolchain := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.7
 common_host_c_includes := $(common_c_includes) $(toolchain)/include
